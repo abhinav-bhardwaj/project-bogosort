6b6021e9bbb5fac0496cbce16acd6658ecc79f98
Abhinav Dubey <abhi@Mac.fritz.box>
2026-05-12 02:08:57 +0200

    Landing page and About us Page

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,11 +1,18 @@
 from flask import Blueprint, render_template
-#from flask import redirect, url_for
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
+def landing():
+    return render_template("landing.html")
+
+@main.route("/analyze/")
 def index():
     return render_template("index.html")
+
+@main.route("/about/")
+def about():
+    return render_template("about.html")
 
 @main.route("/articles/<article_id>/")
 def article_page(article_id):
